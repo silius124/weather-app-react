8ac75a37914e88ba3d48cf28429f7bc1ad8d2cc7
Danil <silius12528@gmail.com>
2025-08-09 20:27:34 +0300

    add github actions yml

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/weather-react-app/",
+  base: "/weather-app-react/",
   plugins: [react(), tailwindcss()],
 });
